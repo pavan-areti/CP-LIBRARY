195bde5c357ef1aaa4d0c77fd8321c19f0390cc6
Debadipto Biswas <49875064+JacNikola@users.noreply.github.com>
2021-10-15 19:09:58 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,6 +1,9 @@
-void dfs(int node, vec<bool> &vis, vec<vec<int>> &g) {
+vec<int> vis(mxN);
+vec<vec<int>> g(mxN);
+
+void dfs(int node) {
 	vis[node] = 1;
 	for(auto to : g[node]) {
-		if(!vis[to]) dfs(to, vis, g);
+		if(!vis[to]) dfs(to);
 	}
 }
