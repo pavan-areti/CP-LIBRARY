f8d26664ecae5ef000b9c738ce025de7eec62753
Debadipto Biswas <49875064+JacNikola@users.noreply.github.com>
2021-10-15 19:07:49 +0530

    Update DFS-GRID.cpp

--- DFS-GRID.cpp
+++ DFS-GRID.cpp
@@ -1,7 +1,10 @@
-void dfs(int x, int y, vec<vec<bool>> &vis, vec<vec<char>> &g) {
+vec<vec<int>> vis(mxN);		
+vec<vec<char>> g(mxN);
+
+void dfs(int x, int y) {
     vis[x][y] = 1;
     rep(i, 0, 3) {
         if(!vis[x + dx[i]][y + dy[i]] and g[x + dx[i]][y + dy[i]] == '.') 
-        dfs(x + dx[i], y + dy[i], vis, g);
+        dfs(x + dx[i], y + dy[i]);
     }
 }
