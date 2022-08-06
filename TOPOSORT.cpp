5e13452bf7b5ce4e73ef5971634387e38aefb0ed
Debadipto Biswas <49875064+JacNikola@users.noreply.github.com>
2022-08-07 03:18:59 +0530

    Update TOPOSORT.cpp

--- TOPOSORT.cpp
+++ TOPOSORT.cpp
@@ -1,14 +1,20 @@
-vec<int> topo_sort(const vec<vec<int>> &g) {
-    vec<int> indeg(sz(g)), ret;
-    rep(i, 1, sz(g) - 1) for(int x : g[i]) indeg[x]++;
+vector<int> topo_sort(const vector<vector<int>> &g) {
+    vector<int> indeg(g.size()), ret;
+    for(int i = 1; i < (int)g.size(); i ++)
+        for(int v : g[i]) indeg[v] ++;
+
     queue<int> q; // use priority_queue for lexic. largest ans.
-    rep(i, 1, sz(g) - 1) if(indeg[i] == 0) q.push(i);
+
+    for(int i = 1; i < (int)g.size(); i ++)
+        if(indeg[i] == 0) q.push(i);
+
     while(!q.empty()) {
         int i = q.front(); // top() for priority queue
         ret.push_back(i);
         q.pop();
         for(int x : g[i])
-            if (--indeg[x] == 0) q.push(x);
+            if(--indeg[x] == 0) q.push(x);
     }
+
     return ret;
 }
