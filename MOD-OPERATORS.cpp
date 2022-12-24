fa8de038cd3aec922bd4ab39aa479ec8c5a1cd4d
pavan-areti <62766354+pavan-areti@users.noreply.github.com>
2022-12-24 22:00:11 +0530

    added factorial

--- MOD-OPERATORS.cpp
+++ MOD-OPERATORS.cpp
@@ -1,9 +1,9 @@
-inline int add(int a, int b) {return (a + b >= mod ? a + b - mod : a + b);}
+inline lli add(lli a, lli b) {return (a + b >= mod ? a + b - mod : a + b);}
 
-inline int mul(int a, int b) {return a * b % mod;}
+inline lli mul(lli a, lli b) {return (a * b) % mod;}
 
-int binpow(int a, int b) {
-    int res = 1;
+lli binpow(lli a, lli b) {
+    lli res = 1;
     a %= mod;
     while(b) {
         if(b & 1) res = mul(res,a);
@@ -13,6 +13,15 @@ int binpow(int a, int b) {
     return res;
 }
 
-int inv(int x) {return binpow(x, mod - 2);}
+lli inv(lli x) {return binpow(x, mod - 2);}
 
-inline int divide(int x, int y) {return mul(x, inv(y));}
+lli modFact(lli n)
+{
+    lli ans = 1;
+    for(int i=1;i<=n;i++){
+        ans = mul(ans,i);
+    }
+    return ans%mod;
+}
+    
+inline lli divide(lii x, lli y) {return mul(x, inv(y));}
