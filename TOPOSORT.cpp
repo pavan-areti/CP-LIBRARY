270db2692de21718d1d162849014c77204d7d6f0
pavan-areti <62766354+pavan-areti@users.noreply.github.com>
2022-12-25 20:01:54 +0530

    added cycle checking to toposort

--- TOPOSORT.cpp
+++ TOPOSORT.cpp
@@ -18,3 +18,32 @@ vector<int> topo_sort(const vector<vector<int>> &g) {
 
     return ret;
 }
+
+
+
+
+// topo sort check for cycles and also ned to pass size n as 2nd parameter
+vector<int> topo(const unordered_set<int> g[],int n) {
+    int k = n;
+    int cnt = n;
+    vector<int> indeg(k), ret;
+    
+    for(int i = 0; i < k; i ++)
+        for(int v : g[i]) indeg[v] ++;
+
+    queue<int> q;
+
+    for(int i = 0; i < k; i ++)
+        if(indeg[i] == 0) q.push(i);
+
+    while(!q.empty()) {
+        int i = q.front(); // top() for priority queue
+        ret.push_back(i);
+        cnt--;
+        q.pop();
+        for(int x : g[i])
+            if(--indeg[x] == 0) q.push(x);
+    }
+    
+    return cnt==0?ret:vector<int>{};
+}
